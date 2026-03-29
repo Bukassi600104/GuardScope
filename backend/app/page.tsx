ddb919ce4092122dc2f00db9d7694bf5de6f8f61
Tony <tony@guardscope.io>
2026-03-29 14:40:54 +0100

    fix: CSSProperties import from react not next (build error)

--- backend/app/page.tsx
+++ backend/app/page.tsx
@@ -1,4 +1,5 @@
-import type { Metadata, CSSProperties } from 'next'
+import type { Metadata } from 'next'
+import type { CSSProperties } from 'react'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 
